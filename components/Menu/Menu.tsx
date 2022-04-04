17e12d41f4efe0ee82fbce50f34bcc8e7a63a470
Tadeo Aguilar <Tadeo.Aguilar@FINNING.COM>
2022-04-04 19:28:06 -0400

    responsive menu

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,10 +1,10 @@
-import React,{FC} from 'react'
+import React,{FC, useEffect, useState} from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { menu,iconLibrary } from '../../framework/common/types/webpage'
 import style from './Menu.module.css'
 import {List,ListItem} from '@components/ui/List'
-
+import cn from 'classnames'
 
 interface Props {
     menu: menu[],
@@ -17,30 +17,44 @@ const Menu: FC<Props> = ({menu, iconLibrary}) => {
   const Icon =iconLibrary  
   const mainIcon= Icon.filter( (a)=>{return a.fields.name==='mainIcon'})[0].fields.icon.fields.file.url 
   const hamburgerMenu= Icon.filter( (a)=>{return a.fields.name==='hamburgerMenu'})[0].fields.icon.fields.file.url   
+ const [btnOpen,setbtnOpen] = useState(false)
+  const clickHandler = () => {
+      setbtnOpen(!btnOpen)
+      
+  }
+
+
 
   return (
       <>
 
-            <div className= {style.container} >                
-                <nav className= {style.navbar} >     
+            <div className= {style.container} >  
                     <div className={style.left}>
                         {mainIcon && <Image src={`https:${mainIcon}` } alt="Tadeo Logo"  width={250} height={80}/>}
-                    </div>       
+                    </div>               
+                <nav className= {!btnOpen ? style.navbar_hidden : style.navbar} >     
+                          
                     <div className={style.right}>
                         <li className={style.List}>   
                             {menuItems.filter((a,i)=> {return a.fields.position === 'Left'}).map((e,i) => (
-                            <ul className={style.ListItem} key={i.toString()}  >                        
+                            <ul className={style.ListItem} key={i.toString()}  onClick={clickHandler}  >                        
                                 <Link href = {e.fields.link}><a  className={style.label}> {e.fields.name} </a></Link>                        
                             </ul>
                             ))}
                             <button key={5} type="button" className={style.btn}>Red</button>    
                         </li>                        
-                        <div className={style.hamburger}>
-                            <span className="material-icons">&#xe5d2;</span>
-                        </div>
+                       
                     </div>
-
                 </nav>
+                    <div className={style.hamburger}>
+                            <div className={btnOpen? ' hidden':' inline-block absolute z-50'} >
+                                <span className="material-icons" id='menu' onClick={clickHandler}>&#xe5d2;</span>
+                            </div>
+                            <div className={!btnOpen? ' hidden':' inline-block absolute z-50'} >
+                                <span className="material-icons" id='close' onClick={clickHandler}>&#xe5cd;</span>
+                            </div>
+                    </div>          
+               
             </div>
 
 
