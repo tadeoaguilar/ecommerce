6b36110e79e967649ee52f019e7440b183df3432
Tadeo Aguilar <tadeo.aguilar@finning.com>
2022-04-18 21:01:55 -0400

    Product Card 2

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -15,7 +15,7 @@ const ProductCard: FC<Props> = ({products }) => {
     
     console.log(products)
   return (
-    <div className= ' grid grid-cols-3 gap-10 w-8/12 mx-auto'>
+    <div className= ' grid grid-cols-1 md:grid-cols-3 gap-10 w-8/12 mx-auto'>
         { products.map((product) => {
             const {images,options} = product
             console.log(images)
